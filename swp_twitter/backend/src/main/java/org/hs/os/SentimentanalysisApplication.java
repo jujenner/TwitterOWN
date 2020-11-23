c1f4c8e2445ec96bcc0197671b42c436d2c27a81
Christof Wolke <cwolke@slashwhy.de>
2020-11-23 14:22:22 +0100

    using modules

--- swp_twitter/backend/src/main/java/org/hs/os/SentimentanalysisApplication.java
+++ swp_twitter/backend/src/main/java/org/hs/os/SentimentanalysisApplication.java
@@ -1,12 +1,13 @@
-package backend;
+package org.hs.os;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class SentimentanalysisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SentimentanalysisApplication.class, args);
     }
+
 }
