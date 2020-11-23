07d10144d8b65960a3a418a2a9c02c63b0d58fd7
Christof Wolke <cwolke@slashwhy.de>
2020-11-23 13:57:16 +0100

    created SentimentanalysisApplication.java

---
+++ swp_twitter/src/main/java/backend/SentimentanalysisApplication.java
@@ -0,0 +1,12 @@
+package backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentanalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentimentanalysisApplication.class, args);
+    }
+}
