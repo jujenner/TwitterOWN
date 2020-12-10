7875b11a7434ae0145684e7216bd763f43ecd93a
Julian Jenner <>
2020-12-10 15:05:57 +0100

    vergeblicher Versuch Scheduling Tasks

--- swp_twitter/backend/src/main/java/org/hs/os/SentimentanalysisApplication.java
+++ swp_twitter/backend/src/main/java/org/hs/os/SentimentanalysisApplication.java
@@ -2,12 +2,15 @@ package org.hs.os;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication()
+@EnableScheduling
 public class SentimentanalysisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SentimentanalysisApplication.class, args);
+        System.out.print("Dalli stinkt");
     }
 
 }
