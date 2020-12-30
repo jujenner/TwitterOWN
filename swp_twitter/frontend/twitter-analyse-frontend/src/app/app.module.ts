1e66491d062ecb9d066a45f6bf838f148073742c
Christof Wolke <cwolke@slashwhy.de>
2020-12-30 10:48:22 +0100

    added material support

--- swp_twitter/frontend/twitter-analyse-frontend/src/app/app.module.ts
+++ swp_twitter/frontend/twitter-analyse-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MaterialModule} from './material-module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
