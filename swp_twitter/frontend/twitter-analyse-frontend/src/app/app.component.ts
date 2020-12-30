cf79b4d0b294fd881371a7a79da28ebbbe7ba5f5
Julian Jenner <>
2020-12-30 12:43:25 +0100

    startseite

--- swp_twitter/frontend/twitter-analyse-frontend/src/app/app.component.ts
+++ swp_twitter/frontend/twitter-analyse-frontend/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'twitter-analyse-frontend';
+
+  bildLogo: string ='assets/images/Logo.PNG';
 }
--- swp_twitter/frontend/twitter-analyse-frontend/src/app/app.module.ts
+++ swp_twitter/frontend/twitter-analyse-frontend/src/app/app.module.ts
@@ -5,16 +5,22 @@ import {MaterialModule} from './material-module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FinanceFeedAnlegenComponent } from './finance-feed-anlegen/finance-feed-anlegen.component';
+import { FinanceFeedsAnzeigenComponent } from './finance-feeds-anzeigen/finance-feeds-anzeigen.component';
+import { AnalyseErgebnisComponent } from './analyse-ergebnis/analyse-ergebnis.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FinanceFeedAnlegenComponent,
+    FinanceFeedsAnzeigenComponent,
+    AnalyseErgebnisComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
